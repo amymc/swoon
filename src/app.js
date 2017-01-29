7c4c6a13966afa9b82fd8492aa3b1cded69a8d89
amymc <amy.mccarthy2@mail.dcu.ie>
2017-01-29 22:17:24 +0000

    project setup

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './app.css';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      showPopup: true
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
