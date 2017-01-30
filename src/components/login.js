503d510e101444c24a728328f97572da080edccc
amymc <amy.mccarthy2@mail.dcu.ie>
2017-01-30 18:23:44 +0000

    convert login modal to form

--- src/components/login.js
+++ src/components/login.js
@@ -11,7 +11,7 @@ class Login extends React.Component {
     };
 
     this.onRegisterClick = this.onRegisterClick.bind(this);
-    this.onLoginClick = this.onLoginClick.bind(this);
+    this.onLoginSubmit = this.onLoginSubmit.bind(this);
     this.renderLogin = this.renderLogin.bind(this);
     this.validateEmail = this.validateEmail.bind(this);
   }
@@ -24,14 +24,13 @@ class Login extends React.Component {
     });
   }
 
-  onLoginClick(e) {
+  onLoginSubmit(e) {
     e.preventDefault();
 
     this.validateEmail();
   }
 
   validateEmail() {
-    console.log('validate');
     // eslint-disable-next-line
     const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
 
@@ -51,17 +50,19 @@ class Login extends React.Component {
     return(
       <div className="login__inner-wrapper">
         <h1 className="login__title">Log in</h1>
-        <div className="login__error-wrapper">
-          {this.state.invalidEmail ?
-            <p className="login__text login__text--error">A valid email is required</p> :
-            null
-          }
-        </div>
-        <input className={"login__input" + (this.state.invalidEmail ? " login__input--error" : "")} value={this.state.email} type="email" placeholder="E-mail address" onChange={event => this.setState({email: event.target.value})}/>
-        <button className="button button--standard" onClick={(e) => this.onLoginClick(e)}>
-          {this.state.showSignup ? 'Sign up' : 'Log in'}
-        </button> 
-        <button className="button button--text" onClick={(e) => this.onRegisterClick(e)}>Don't have an account? Register here</button>
+        <form className="modal__form" onSubmit={(e) => this.onLoginSubmit(e)} >
+          <div className="login__error-wrapper">
+            {this.state.invalidEmail ?
+              <p className="login__text login__text--error">A valid email is required</p> :
+              null
+            }
+          </div>
+          <input className={"login__input" + (this.state.invalidEmail ? " login__input--error" : "")} value={this.state.email} type="email" placeholder="E-mail address" onChange={event => this.setState({email: event.target.value})}/>
+          <button className="button button--standard" type="submit">
+            {this.state.showSignup ? 'Sign up' : 'Log in'}
+          </button> 
+          <button className="button button--text" onClick={(e) => this.onRegisterClick(e)}>Don't have an account? Register here</button>
+        </form>
       </div>
     );
   }
