d6030bd2d270806c6a3b6ae15da80dfaf21ff554
amymc <amy.mccarthy2@mail.dcu.ie>
2017-01-30 12:22:50 +0000

    login module layout

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,18 @@
 import React from 'react';
-//import '../styles/modules/login.scss';
+import '../styles/modules/login.css';
 
 class Login extends React.Component {
 
   render() {
     return (
-      <div> LOGIN </div>
+      <div className="login">
+        <div className="login__inner-wrapper">
+          <h1 className="login__title">Log in</h1>
+          <input className="login__input" type="email" placeholder="E-mail address"/>
+          <button className="button">Log in</button>
+          <p className="login__text">Don't have an account? Register here</p>
+        </div>
+      </div>
     );
   }
 }
