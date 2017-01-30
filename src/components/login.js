e39f9c5258bab23e1276fc4b83a0df3b734865f2
amymc <amy.mccarthy2@mail.dcu.ie>
2017-01-30 13:05:41 +0000

    add email validation

--- src/components/login.js
+++ src/components/login.js
@@ -5,10 +5,13 @@ class Login extends React.Component {
    constructor(props) {
     super(props);
     this.state = {
+      invalidEmail: false,
       showSignup: false
     };
 
     this.onRegisterClick = this.onRegisterClick.bind(this);
+    this.onSignupClick = this.onSignupClick.bind(this);
+    this.validateEmail = this.validateEmail.bind(this);
   }
 
   onRegisterClick(e) {
@@ -19,18 +22,48 @@ class Login extends React.Component {
     });
   }
 
+  onSignupClick(e) {
+    e.preventDefault();
+
+    this.validateEmail();
+
+    //  this.setState({
+    //   showSignup: true
+    // });
+  }
+
+  validateEmail() {
+    // eslint-disable-next-line
+    const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    if (!re.test(this.state.email)) {
+      this.setState({
+        invalidEmail: true
+      });
+    } else {
+      this.setState({
+        invalidEmail: false
+      });
+    }
+  }
 
   render() {
     return (
       <div className="login">
         <div className="login__inner-wrapper">
           <h1 className="login__title">Log in</h1>
-          <input className="login__input" type="email" placeholder="E-mail address"/>
+          <div className="login__error-wrapper">
+            {this.state.invalidEmail ?
+              <p className="login__error">A valid email is required</p> :
+              null
+            }
+          </div>
+          <input className={"login__input" + (this.state.invalidEmail ? " login__input--error" : "")} type="email" placeholder="E-mail address"/>
           {this.state.showSignup ?
-            <button className="button">Sign up</button> :
-            <button className="button">Log in</button>
+            <button className="button button--standard" onClick={(e) => this.onSignupClick(e)}>Sign up</button> :
+            <button className="button button--standard">Log in</button>
           }
-          <p className="login__text" onClick={(e) => this.onRegisterClick(e)}>Don't have an account? Register here</p>
+          <button className="button button--text" onClick={(e) => this.onRegisterClick(e)}>Don't have an account? Register here</button>
         </div>
       </div>
     );
