ed50a2714f0ab95f6f41f73dc1899a65b52fade4
amymc <amy.mccarthy2@mail.dcu.ie>
2017-01-30 00:44:10 +0000

    convert popup to statless component

--- src/components/popup.js
+++ src/components/popup.js
@@ -1,17 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import '../styles/modules/popup.css';
 
-class Popup extends Component {
-
-  render() {
-    return (
-      <div className="popup">
-        <span className="popup__close-btn" onClick={(e) => this.props.onClick(e)}>X</span>
-        <h1 className="popup__title">Heading</h1>
-        <p className="popup__body">Sed lobortis justo vitae nisi fermentum, dapibus dictum eros malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce vitae tellus et purus faucibus vehicula. Morbi felis tellus, ultrices vel pellentesque ut, efficitur vitae orci. Quisque sit amet sem vitae neque molestie finibus sit amet suscipit leo. Vivamus mattis sagittis blandit. Maecenas eget tortor blandit, venenatis ipsum ac, semper tortor.</p>
-      </div>
-    );
-  }
+const Popup = () => {
+  return (
+    <div className="popup">
+      <span className="popup__close-btn" onClick={(e) => this.props.onClick(e)}>X</span>
+      <h1 className="popup__title">Heading</h1>
+      <p className="popup__body">Sed lobortis justo vitae nisi fermentum, dapibus dictum eros malesuada. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce vitae tellus et purus faucibus vehicula. Morbi felis tellus, ultrices vel pellentesque ut, efficitur vitae orci. Quisque sit amet sem vitae neque molestie finibus sit amet suscipit leo. Vivamus mattis sagittis blandit. Maecenas eget tortor blandit, venenatis ipsum ac, semper tortor.</p>
+    </div>
+  );
 }
 
 export default Popup;
