3278b2b1d830c7259254773972baeb83fafd5bc8
amymc <amy.mccarthy2@mail.dcu.ie>
2017-01-30 12:35:35 +0000

    show signup button

--- src/components/login.js
+++ src/components/login.js
@@ -2,6 +2,23 @@ import React from 'react';
 import '../styles/modules/login.css';
 
 class Login extends React.Component {
+   constructor(props) {
+    super(props);
+    this.state = {
+      showSignup: false
+    };
+
+    this.onRegisterClick = this.onRegisterClick.bind(this);
+  }
+
+  onRegisterClick(e) {
+    e.preventDefault();
+
+     this.setState({
+      showSignup: true
+    });
+  }
+
 
   render() {
     return (
@@ -9,8 +26,11 @@ class Login extends React.Component {
         <div className="login__inner-wrapper">
           <h1 className="login__title">Log in</h1>
           <input className="login__input" type="email" placeholder="E-mail address"/>
-          <button className="button">Log in</button>
-          <p className="login__text">Don't have an account? Register here</p>
+          {this.state.showSignup ?
+            <button className="button">Sign up</button> :
+            <button className="button">Log in</button>
+          }
+          <p className="login__text" onClick={(e) => this.onRegisterClick(e)}>Don't have an account? Register here</p>
         </div>
       </div>
     );
